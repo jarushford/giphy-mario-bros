38af16d957639a11968a32629ee178ab6d437619
Jamie Rushford <jar.rushford@gmail.com>
2019-01-03 12:08:34 -0700

    Set up Welcome Page

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../main.scss'
+
+export default function Welcome() {
+  return (
+    <section className="welcome">
+      <h1 className="title">Giphy Mario Bros.</h1>
+      <p className="instructions">To play this game, select your number of players. On each players turn they will be given a caption. Every other player must choose a GIF to match that caption when they are prompted to do so. Once everyone has chosen, the player whose turn it is will choose their favorite and that player will be awarded coins. Then it will move to the next player. The game starts with Player 1!</p>
+      <button className="start-btn">Start</button>
+    </section>
+  )
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Welcome from '../../components/Welcome/Welcome'
 import '../../main.scss'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Route exact path='/' component={Welcome} />
       </div>
     )
   }
